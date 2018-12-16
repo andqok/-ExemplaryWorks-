4cf0d7c17e7c0d4b9e511bad129cf4b233444318
Andrew Domanski <andqok@gmail.com>
2018-12-16 05:32:40 +0200

    remove old code

---
+++ scripts/classify.js
@@ -0,0 +1,9 @@
+function renderHex(parent) {
+    let hexList = [
+        '0', '1', '2', '3', '4', '5', '6', '7', 
+        '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
+}
+
+function getHexStats() {
+    
+}
